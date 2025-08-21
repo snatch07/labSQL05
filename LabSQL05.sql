3c8acb65084a3edf7af8e2eb7f79103162c47b22
Ratthaphoom Dongyasopha <ratthaphoom8230@gmail.com>
2025-08-21 10:07:04 +0700

    Update LabSQL05.sql

--- LabSQL05.sql
+++ LabSQL05.sql
@@ -31,4 +31,38 @@ from products as p join Suppliers as s
 on p.SupplierID = s.SupplierID
 
 
+select companyName,OrderID
+from orders as o,shippers as s
+where .ShipperID =o.shipvia
+and orderID = 10275
 
+select companyName,orderID
+from orders o join shippers s
+on s.ShipperID = o.ShipVia
+where orderID = 10275
+
+-- ต้องการรหัสพนักงาน ชื่อพนักงาน รหัสใบสั่งซื้อที่เกี่ยวข้อง เรียงตามลำดับพนักงาน
+select e.EmployeeID,firstName,orderID
+from Employees as e join orders as o
+on e.EmployeeID = o.EmployeeID
+order by EmployeeID
+
+-- ต้องการรหัสสินค้า เมือง และประเทศของผู้จำหน่าย
+select productID,ProductName,city,country
+from products p join suppliers s 
+on p.supplierID = s.SupplierID
+
+-- ต้องการชื่อบรริษัทขนส่ง และจำนวนใบสั่งซื้อที่เกี่ยวข้อง
+select companyName, count(*)
+from orders as o join shippers as s
+on o.shipvia = s.ShipperID
+group by companyName
+
+-- ต้องการรหัสสินค้า ชื่อสินค้า และจำนวนทั้งหมดที่ขายได้
+select p.productID,p.productName,sum(Quantity)
+from products p join [order details] od
+on p.productID = od.productID
+group by p.productID,p.productName
+order by 1
+
+-- 
