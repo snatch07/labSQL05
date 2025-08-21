6f0ba09265c938f9c16c0f9b4caeafbc381db02e
Ratthaphoom Dongyasopha <ratthaphoom8230@gmail.com>
2025-08-21 11:17:16 +0700

    Update LabSQL05.sql

--- LabSQL05.sql
+++ LabSQL05.sql
@@ -65,4 +65,50 @@ on p.productID = od.productID
 group by p.productID,p.productName
 order by 1
 
--- 
+
+select O.orderID เลขใบสั่งซื้อ,c.companyName ลูกค้า,e.firstName พนักงาน,o.shipAddress ส่งไปที่
+from orders o ,customers c,employees e
+where o.customerID = c.customerID and o.EmployeeID = e.EmployeeID
+
+select O.orderID เลขใบสั่งซื้อ,c.companyName ลูกค้า,e.firstName พนักงาน,o.shipAddress ส่งไปที่
+from orders o join customers c on o.customerID = c.customerID
+			  join employees e on o.employeeID = e.employeeID
+
+--
+select e.employeeID,firstName,count(*) as[นวน order],sum(freight) as [Sum of Freight]
+from employees e join orders o 
+on e.EmployeeID = o.employeeID
+where year(orderdate) = 1998
+group by e.employeeID,firstName	
+
+-- ต้องการรหัสสินค้า ชื่อสินค้า ที่ nancy ขายได้ ทั้งหมด เรียงตามลำดับรหัสสินต้า
+select distinct p.productID,p.productName
+from employees e join orders o on e.employeeID = o.employeeID
+				  join [Order Details] od on o.orderID = od.orderID
+				  join products p on od.productID = p.productID
+where e.firstName = 'Nancy'
+order by productID
+
+-- ต้องการชื่อบริษัทลูกค้า Around the Horn ซื้อสินค้าที่มาจากประเทศอะไรบ้าง
+select distinct s.country
+from customers c join orders o on c.customerID = o.customerID
+				 join [Order Details] od on o.orderID = od.orderID
+				 join products p on od.productID = p.productID
+				 join suppliers s on s.supplierID = p.supplierID
+where c.companyName = 'Around the Horn'
+
+-- บริษัทลูกค้าชื่อ Around the Horn ซิ้อสินค้าอะไรบ้าง จำนวนเท่าใด
+select p.productID,p.productName,sum(Quantity) as [sum of Quantity]
+from customers c join orders o on c.customerID = o.customerID
+				 join [Order Details] od on o.orderID = od.orderID
+				 join products p on p.productID = od.productID
+where c.companyName = 'Around the Horn'
+group by p.productID,p.productName
+order by 1
+
+-- ต้องการหมายเลขใบสั่งซื้อ ชื่อพนักงาน และยอดขาบในใบสั่งซื้อนั้น
+select o.orderID,e.FirstName,sum((od.Quantity * od.UnitPrice * (1 - od.Discount))) as TotalCash
+from orders o join Employees e on o.EmployeeID = e.employeeID
+			  join [Order Details] od on o.orderID = od.orderID
+group by o.orderID,e.firstName
+order by orderID
